0cee0fbe69e3f7023f79dd498cac236ed9b7a318
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2024-03-12 22:49:54 -0300

    AssignationAST implementation

--- src/main/kotlin/common/ast/implementation/AssignationAST.kt
+++ src/main/kotlin/common/ast/implementation/AssignationAST.kt
@@ -1,7 +1,9 @@
 package common.ast.implementation
 
 import common.ast.AST
+import common.ast.node.LeafNode
 import common.ast.node.Node
+import common.ast.node.TreeNode
 import common.token.Token
 
 
@@ -15,12 +17,21 @@ import common.token.Token
  */
 class AssignationAST(private val tokens: List<Token>) :AST {
 
-
     override fun getChildren(): List<Node> {
-        TODO("Not yet implemented")
+        // Asumo que el primer token es el identifier y el ultimo el punto y coma
+        val identifierToken = tokens.first()
+        val valueTokens = tokens.drop(1).dropLast(2) // Todos los tokens menos el identificador, el velue y el punto y coma
+        val valueToken = tokens.dropLast(1).last() // El último token antes del punto y coma va a ser el valor
+
+        val typeNode = LeafNode(valueToken.type, valueToken.type.name)
+
+        val identifierNode = LeafNode(identifierToken.type, identifierToken.value)
+        val valueNode = TreeNode(valueToken.type, null, null, valueToken.value)
+
+        return listOf(identifierNode, typeNode, valueNode)
     }
 
     override fun getTokensInLine(): List<Token> {
-        TODO("Not yet implemented")
+        return tokens
     }
 }
